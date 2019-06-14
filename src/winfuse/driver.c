676bc504415c57a19ffd7ce51379d10573c6a0c6
Bill Zissimopoulos <billziss@navimatics.com>
2019-06-14 11:19:46 -0700

    winfuse skeleton

---
+++ src/winfuse/driver.c
@@ -0,0 +1,34 @@
+/**
+ * @file winfuse/driver.c
+ *
+ * @copyright 2019 Bill Zissimopoulos
+ */
+/*
+ * This file is part of WinFuse.
+ *
+ * You can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * Licensees holding a valid commercial license may use this software
+ * in accordance with the commercial license agreement provided in
+ * conjunction with the software.  The terms and conditions of any such
+ * commercial license agreement shall govern, supersede, and render
+ * ineffective any application of the AGPLv3 license to this software,
+ * notwithstanding of any reference thereto in the software or
+ * associated repository.
+ */
+
+#include <winfuse/driver.h>
+
+DRIVER_INITIALIZE DriverEntry;
+
+#ifdef ALLOC_PRAGMA
+#pragma alloc_text(INIT, DriverEntry)
+#endif
+
+NTSTATUS DriverEntry(
+    PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+{
+    return STATUS_SUCCESS;
+}
---
+++ src/winfuse/driver.h
@@ -0,0 +1,31 @@
+/**
+ * @file winfuse/driver.h
+ *
+ * @copyright 2019 Bill Zissimopoulos
+ */
+/*
+ * This file is part of WinFuse.
+ *
+ * You can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * Licensees holding a valid commercial license may use this software
+ * in accordance with the commercial license agreement provided in
+ * conjunction with the software.  The terms and conditions of any such
+ * commercial license agreement shall govern, supersede, and render
+ * ineffective any application of the AGPLv3 license to this software,
+ * notwithstanding of any reference thereto in the software or
+ * associated repository.
+ */
+
+#ifndef WINFUSE_DRIVER_H_INCLUDED
+#define WINFUSE_DRIVER_H_INCLUDED
+
+#include <ntifs.h>
+#define WINFSP_SYS_INTERNAL
+#include <winfsp/fsctl.h>
+
+#pragma warning(disable:4100)           /* unreferenced formal parameter */
+
+#endif
