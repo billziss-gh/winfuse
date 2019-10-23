de89a37f2854c2397c701886404effcd996377f6
Bill Zissimopoulos <billziss@navimatics.com>
2019-10-22 21:52:09 -0700

    sys: FuseFileDeviceFini: revert previous and fix use after FuseFree

--- src/winfuse/file.c
+++ src/winfuse/file.c
@@ -32,20 +32,12 @@ VOID FuseFileDeviceInit(PDEVICE_OBJECT DeviceObject)
 VOID FuseFileDeviceFini(PDEVICE_OBJECT DeviceObject)
 {
     FUSE_DEVICE_EXTENSION *DeviceExtension = FuseDeviceExtension(DeviceObject);
-    KIRQL Irql;
-    PLIST_ENTRY Entry;
     FUSE_FILE *File;
 
-    for (;;)
+    for (PLIST_ENTRY Entry = DeviceExtension->FileList.Flink; &DeviceExtension->FileList != Entry;)
     {
-        KeAcquireSpinLock(&DeviceExtension->FileListLock, &Irql);
-        Entry = RemoveHeadList(&DeviceExtension->FileList);
-        File = &DeviceExtension->FileList != Entry ?
-            CONTAINING_RECORD(Entry, FUSE_FILE, ListEntry) : 0;
-        KeReleaseSpinLock(&DeviceExtension->FileListLock, Irql);
-        if (0 == File)
-            break;
-
+        File = CONTAINING_RECORD(Entry, FUSE_FILE, ListEntry);
+        Entry = Entry->Flink;
         FuseFree(File);
     }
 }
