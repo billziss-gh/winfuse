e704e31d04b9ba3e78d811c5041a36b165c19e29
Bill Zissimopoulos <billziss@navimatics.com>
2019-11-07 23:16:03 +0000

    tools: deploy.bat

--- tools/deploy.bat
+++ tools/deploy.bat
@@ -10,23 +10,28 @@ if not X%1==X set TARGET_MACHINE=%1
 set TARGET_ACCOUNT=\Users\%USERNAME%\Downloads\winfuse\
 set TARGET=\\%TARGET_MACHINE%%TARGET_ACCOUNT%
 
-set RegKey="HKLM\SOFTWARE\WinFsp"
-set RegVal="InstallDir"
-reg query !RegKey! /v !RegVal! /reg:32 >nul 2>&1
-if !ERRORLEVEL! equ 0 (
-    for /f "tokens=2,*" %%i in ('reg query !RegKey! /v !RegVal! /reg:32 ^| findstr !RegVal!') do (
-        set InstallDir=%%j
-    )
-)
-if not exist "!InstallDir!" (echo cannot find WinFsp installation >&2 & goto fail)
-
 cd %~dp0..
+
+if exist ext\winfsp\build\VStudio\build\%CONFIG% (
+    set WINFSP=!cd!\ext\winfsp\build\VStudio\build\%CONFIG%\
+) else (
+    set RegKey="HKLM\SOFTWARE\WinFsp"
+    set RegVal="InstallDir"
+    reg query !RegKey! /v !RegVal! /reg:32 >nul 2>&1
+    if !ERRORLEVEL! equ 0 (
+        for /f "tokens=2,*" %%i in ('reg query !RegKey! /v !RegVal! /reg:32 ^| findstr !RegVal!') do (
+            set WINFSP=%%jbin\
+        )
+    )
+    if not exist "!WINFSP!" (echo cannot find WinFsp installation >&2 & goto fail)
+)
+
 mkdir %TARGET% 2>nul
 for %%f in (winfuse-%SUFFIX%.sys winfuse-tests-%SUFFIX%.exe) do (
     copy build\VStudio\build\%CONFIG%\%%f %TARGET% >nul
 )
 for %%f in (winfsp-%SUFFIX%.sys winfsp-%SUFFIX%.dll) do (
-    copy "!InstallDir!bin\%%f" %TARGET% >nul
+    copy "!WINFSP!%%f" %TARGET% >nul
 )
 if exist tst\memfs-fuse3\build\%CONFIG% (
     for %%f in (memfs-fuse3-%SUFFIX%.exe fuse3.dll) do (
