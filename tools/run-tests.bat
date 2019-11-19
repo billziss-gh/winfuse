b894d65ecf50ec8b95703fc1e6ebecb5281460f8
Bill Zissimopoulos <billziss@navimatics.com>
2019-11-19 14:53:28 -0800

    tools: run-tests: memfs: remaining create tests

--- tools/run-tests.bat
+++ tools/run-tests.bat
@@ -116,7 +116,8 @@ cd L: >nul 2>nul || (echo Unable to find drive L: >&2 & goto fail)
 L:
 "C:\Program Files (x86)\WinFsp\bin\winfsp-tests-x64.exe" ^
     --external --resilient ^
-    create_test create_related_test create_sd_test create_notraverse_test create_backup_test create_restore_test
+    create_test create_related_test create_sd_test create_notraverse_test create_backup_test ^
+    create_restore_test create_share_test create_curdir_test create_namelen_test
 if !ERRORLEVEL! neq 0 set TestExit=1
 popd
 taskkill /f /im %1-%2.exe
