e3f31aaf4a5b25da5bbb07ad4e0dba17896771a0
Bill Zissimopoulos <billziss@navimatics.com>
2020-01-22 14:22:19 -0800

    fusermount: add skeleton

---
+++ src/fusermount/fusermount.c
@@ -0,0 +1,25 @@
+/**
+ * @file fusermount.c
+ *
+ * @copyright 2019 Bill Zissimopoulos
+ */
+/*
+ * This file is part of WinFuse.
+ *
+ * You can redistribute it and/or modify it under the terms of the GNU
+ * Affero General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * Licensees holding a valid commercial license may use this software
+ * in accordance with the commercial license agreement provided in
+ * conjunction with the software.  The terms and conditions of any such
+ * commercial license agreement shall govern, supersede, and render
+ * ineffective any application of the AGPLv3 license to this software,
+ * notwithstanding of any reference thereto in the software or
+ * associated repository.
+ */
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
